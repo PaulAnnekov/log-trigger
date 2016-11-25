2de11776fc812f1ca5ca1ea1a4c69efebf2da9cb
Paul Annekov <paul.annekov@gmail.com>
2016-11-25 18:29:42 +0200

    more structurized email

--- trigger.sh
+++ trigger.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 log=/var/log/journald.log
 match="\"PRIORITY\" : \"3\""
-host=exim
-port=25
+email_host=exim
+email_port=25
 sender=journald@sr-server.home.annekov.com
 to=paul.annekov@gmail.com
 cat |
 grep --line-buffered "$match" |
 while read line
 do
-    echo "$line" | mailx -S smtp=$host:$port -s "Error on sr-server" -v -r $sender $to
+    container=`echo "$line" | jq --raw-output '.CONTAINER_NAME'`
+    message=`echo "$line" | jq --raw-output '.MESSAGE'`
+    hostname=`echo "$line" | jq --raw-output '._HOSTNAME'`
+    echo -e "Error:\n$message" | mailx -S smtp=$email_host:$email_port -s "Error on $hostname in container $container" -v -r $sender $to
 done
