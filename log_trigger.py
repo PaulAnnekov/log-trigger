18b2fe1a3598cacf9c9d66a834fc853702f7311d
Paul Annekov <paul.annekov@gmail.com>
2017-11-02 01:09:45 +0200

    added ignore for fail2ban ban actions

--- log_trigger.py
+++ log_trigger.py
@@ -14,7 +14,8 @@ ignore = {'smarthome_home_assistant_1': ["*[[]custom_components.device_tracker.p
                                          "*[[]custom_components.device_tracker.padavan_tracker[]] Can't get connected "
                                          "clients: Some error during request: HTTPConnectionPool(host='192.168.0.21', "
                                          "port=80): Read timed out. (read timeout=1)*",
-                                         "*[[]roomba.roomba.Roomba[]]*\"error\":0,*"]}
+                                         "*[[]roomba.roomba.Roomba[]]*\"error\":0,*"],
+          'fail2ban': ["*fail2ban.actions: WARNING * Ban *"]}
 
 
 def init_logging():
